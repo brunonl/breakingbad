c09d7df89e7286e6f894d9494c8f0900075c848e
Bruno Meireles Souza <brunosouza.bhs@fundep.local>
2020-02-02 12:48:16 -0300

    Adding sass and integrating bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -11,13 +11,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link btn-red"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
